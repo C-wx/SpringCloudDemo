327768cfe33b8952850e2814db5d476147cf6fb9
Cbuc <1404668149@qq.com>
2019-12-24 13:41:07 +0800

    zuul路由网关   commit

---
+++ microservicecloud/microservicecloud-zuul-gateway-9527/src/main/java/cbuc/life/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package cbuc.life.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Explain:
+ * @Author: Cbuc
+ * @Version: 1.0
+ * @Date: 2019/12/23
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
