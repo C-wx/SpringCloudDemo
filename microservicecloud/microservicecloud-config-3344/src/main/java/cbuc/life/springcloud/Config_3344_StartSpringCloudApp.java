7f1d0f630721d43db51187bf6a8c63f7187774cc
Cbuc <1404668149@qq.com>
2019-12-25 09:27:51 +0800

    SpringCloud Config服务端配置

---
+++ microservicecloud/microservicecloud-config-3344/src/main/java/cbuc/life/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package cbuc.life.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Explain:
+ * @Author: Cbuc
+ * @Version: 1.0
+ * @Date: 2019/12/24
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
+    }
+}
---
+++ microservicecloud/microservicecloud-config-client-3355/src/main/java/cbuc/life/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,17 @@
+package cbuc.life.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Explain:
+ * @Author: Cbuc
+ * @Version: 1.0
+ * @Date: 2019/12/24
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+}
